c535ba1f923160e2443b076dabdb120e78d9ac2e
Howard <haoy2001@gmail.com>
2020-08-31 03:08:08 -0400

    Switch to using Webpack and TypeScript

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "none",
+  entry: "./src/app.ts",
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|gif|mp3|webm|wav)$/,
+        use: "file-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+      favicon: "./src/assets/favicon.ico",
+    }),
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+  },
+  performance: {
+    hints: false,
+  },
+};
